4828aa32b3a19a9ad140fde22900006924fb5b09
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-27 16:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
-
+	const boldElements = document.querySelectorAll('strong');
+	boldElements.forEach((element) => {
+		element.style.color = 'green';
+	});
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+    //Write your code hereconst bo
+	const boldElements = document.querySelectorAll('strong');
+	boldElements.forEach((element) =>{
+		element.style.color = 'black';
+	});
 }
+const link = document.getElementById('bold-high-link');
+link.addEventListener('mouseover',highlight);
+link.addEventListener('mouseout', return_normal);
+
+
